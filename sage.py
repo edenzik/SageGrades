853775b7a7a4f2261a28ce8c01d46ceee68b59a1
Eden Zik <edenzik@gmail.com>
2015-05-09 10:48:03 -0400

    bug fix

--- sage.py
+++ sage.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-BRANDEIS_ID = '??????'                 #Your Brandeis username (keep safe!)
-BRANDEIS_PASSWORD = '??????'     #Your Brandeis password (keep safe!)
+BRANDEIS_ID = '????' #Your Brandeis username (keep safe!)
+BRANDEIS_PASSWORD = '?????'     #Your Brandeis password (keep safe!)
 SEMESTER = 1                            #Semester count from the most recent (0th semester is the most recent one after registration, 1st is the one before that, etc.)
 SPEED_CONSTANT = 5                      #Number of seconds to wait for page redirection
 
@@ -16,80 +16,83 @@ def get_grades():
 
     browser = webdriver.Firefox()
 
-#Sage Login Page
+    #Sage Login Page
 
     browser.get('https://www.brandeis.edu/sage/')
 
-#Acquiring Signin Elements
+    #Acquiring Signin Elements
 
     userid = browser.find_element_by_id('userid')
 
     pwd = browser.find_element_by_id('pwd')
 
-#Signing In
+    #Signing In
 
     userid.send_keys(BRANDEIS_ID)
 
     pwd.send_keys(BRANDEIS_PASSWORD + Keys.RETURN)
 
-#Waiting for SAGE to load
+    #Waiting for SAGE to load
 
     time.sleep(SPEED_CONSTANT)
 
-#Getting "class schedule" element
+    #Getting "class schedule" element
 
     try:
 
         schdl = browser.find_element_by_id('DERIVED_SSS_SCR_SSS_LINK_ANCHOR2')
 
+        #Going to schedule
+        schdl.click()
+
     except:
         print "Page failed to load, try increasing your speed constant"
+        return
 
-#Going to schedule
+    #Frame switch to context frame
 
-    schdl.click()
-
-#Frame switch to context frame
 
     browser.switch_to_frame('TargetContent')
 
-#Gets semester list of inputs
+    #Gets semester list of inputs
 
     term = browser.find_elements_by_id('SSR_DUMMY_RECV1$sels$0')
 
-#Clicks on the current semester
+    #Clicks on the current semester
 
     term[SEMESTER].click()
 
-#Finds continue button
+    #Finds continue button
 
     cont = browser.find_element_by_id('DERIVED_SSS_SCT_SSR_PB_GO')
 
-#Continues to grades page
+    #Continues to grades page
 
     cont.click()
 
-#Waits for page to load
+    #Waits for page to load
 
     time.sleep(SPEED_CONSTANT)
 
-#Gets table of course titles
+    #Gets table of course titles
+
+    grade_string = ''
 
     try:
 
         courses = browser.find_elements_by_class_name('PAGROUPDIVIDER')
 
+        #Iterates and print every course and its grade
+
+        for idx, val in enumerate(courses):
+            course = val.text
+            grade = browser.find_element_by_id('DERIVED_REGFRM1_CRSE_GRADE_OFF$'+str(idx)).text
+            grade_string+= '{0:60} : {1}\n'.format(course,grade)
+
     except:
         print "Page failed to load, try increasing your speed constant"
+        return
 
-#Iterates and print every course and its grade
-
-    grade_string = ''
-
-    for idx, val in enumerate(courses):
-        course = val.text
-        grade = browser.find_element_by_id('DERIVED_REGFRM1_CRSE_GRADE_OFF$'+str(idx)).text
-        grade_string+= '{0:60} : {1}\n'.format(course,grade)
 
     return grade_string
 
